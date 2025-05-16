d38cbe135112b0633961d7f6bf72275682b5b3ac
Joona Ansamaa <64829395+keravanhurja@users.noreply.github.com>
2025-05-16 05:49:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# My Scroll Animation Project
+
+A simple and responsive personal web page built with HTML, CSS, and JavaScript.
+
+## 🚀 Features
+
+- Smooth scrolling and basic animations
+
+## 🛠️ Built With
+
+- HTML
+- CSS
+- JavaScript (GSAP)
---
+++ sketch.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollSmoother)
+
+const splitTypes = document.querySelectorAll(".reveal-type")
+
+splitTypes.forEach((char, i) => {
+
+    const text = new SplitType(char, {types: "chars"})
+
+    gsap.from(text.chars, {
+        scrollTrigger: {
+            trigger: char,
+            start: "top 80%",
+            end: "top 20%",
+            scrub: true,
+        },
+        y: 100,
+        opacity: 0,
+        stagger: 0.1,
+    })
+})
+
+ScrollSmoother.create({
+  smooth: 1,
+  effects: true,
+});
